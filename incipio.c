761663ce6ff8162df5b684c749ec98a32ca1be7d
Mitch Tishmack <mitch.tishmack@gmail.com>
2014-03-13 07:17:10 -0500

    Add in yuck file setup so make distcheck works.

--- incipio.c
+++ incipio.c
@@ -1,1 +1,13 @@
-int main(void) { puts("success"); }
+#include <stdio.h>
+#include "incipio.yucc"
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    puts("Nothing to do! Re-run with -h or --help for more options.");
+    return 1;
+  } else {
+    yuck_t argp[1];
+    yuck_parse(argp, argc, argv);
+    yuck_free(argp);
+  }
+}
